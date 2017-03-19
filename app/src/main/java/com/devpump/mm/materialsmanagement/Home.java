fed51a2b91d95af1c815199d749c38446adb522d
Matt Fleming <devpump@gmail.com>
2017-03-18 21:35:35 -0400

    Removed unit test files for now; Working JSONObject Request

--- app/src/main/java/com/devpump/mm/materialsmanagement/Home.java
+++ app/src/main/java/com/devpump/mm/materialsmanagement/Home.java
@@ -20,9 +20,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -37,28 +41,14 @@ public class Home extends AppCompatActivity {
     Spinner spin_itemQuantityType;
     ArrayAdapter<CharSequence> adapter;
 
-//    String barCode, itemName, itemQuantity;
-//    int itemSelectedPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        linkComponents();
-//        initializeGlobalVariables();
+        linkInitalizeComponents();
 
-        adapter = ArrayAdapter.createFromResource(
-                this, R.array.string_itemQuantities, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spin_itemQuantityType.setAdapter(adapter);
-
-        getSavedState(savedInstanceState);
+        getPreviousSavedState(savedInstanceState);
     }
-//    public void initializeGlobalVariables(){
-//        barCode = "";
-//        itemName = "";
-//        itemQuantity = "";
-//        itemSelectedPosition = 0;
-//    }
 
     protected void onSaveInstanceState(Bundle outState){
         outState.putString("barCode", tv_scanContent.getText().toString());
@@ -67,56 +57,42 @@ public class Home extends AppCompatActivity {
         outState.putInt("itemQuantityType",spin_itemQuantityType.getSelectedItemPosition());
     }
 
-    public void linkComponents(){
-        tv_scanContent = (TextView)findViewById(R.id.tv_barcode);
-        et_itemName = (EditText)findViewById(R.id.et_itemName);
-        et_itemQuantity = (EditText)findViewById(R.id.et_itemQuantity);
-        spin_itemQuantityType = (Spinner) findViewById(R.id.spin_quantityType);
-    }
-
-    public void getSavedState(Bundle instanceState){
+    public void getPreviousSavedState(Bundle instanceState){
         if(instanceState != null) {
             tv_scanContent.setText(instanceState.getString("barCode"));
             et_itemName.setText(instanceState.getString("itemName"));
             et_itemQuantity.setText(instanceState.getString("itemQuantity"));
             spin_itemQuantityType.setSelection(instanceState.getInt("itemQuantityType"));
+            //Set Edit text cursor to end of line.
+            et_itemName.setSelection(et_itemName.getText().length());
         }
+    }
+
+    public void linkInitalizeComponents(){
+        tv_scanContent = (TextView)findViewById(R.id.tv_barcode);
+        et_itemName = (EditText)findViewById(R.id.et_itemName);
+        et_itemQuantity = (EditText)findViewById(R.id.et_itemQuantity);
+        spin_itemQuantityType = (Spinner) findViewById(R.id.spin_quantityType);
+
+        adapter = ArrayAdapter.createFromResource(
+                this, R.array.string_itemQuantities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin_itemQuantityType.setAdapter(adapter);
     }
 
 
     public void scanBar(View v) {
         try {
-            //start the scanning activity from the com.google.zxing.client.android.SCAN intent
-            Intent intent = new Intent(ACTION_SCAN);
-            //intent.putExtra("SCAN_MODE"); Ignore specific mode type.
-            startActivityForResult(intent, 0);
+            //Start the scanning activity from the com.google.zxing.client.android.SCAN intent
+            Intent scanIntent = new Intent(ACTION_SCAN);
+           // startActivityForResult(scanIntent, 0);
         } catch (ActivityNotFoundException anfe) {
             //on catch, show the download dialog
-            showDialog(Home.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+            Toast alertNoScanner = Toast.makeText(Home.this,"No Scanner Found", Toast.LENGTH_LONG);
+            alertNoScanner.show();
+
         }
-    }
-
-    //alert dialog for downloadDialog
-    private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
-        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
-        downloadDialog.setTitle(title);
-        downloadDialog.setMessage(message);
-        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                try {
-                    act.startActivity(intent);
-                } catch (ActivityNotFoundException anfe) {
-
-                }
-            }
-        });
-        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialogInterface, int i) {
-            }
-        });
-        return downloadDialog.show();
+        checkDatabase();
     }
 
     //on ActivityResult method
@@ -134,4 +110,40 @@ public class Home extends AppCompatActivity {
         }
     }
 
+    public void checkDatabase() { //String barCode, String itemName, double itemQuantity){
+        JSONObject job = new JSONObject();
+        try {
+            job.put("typeOfAction","5sdf");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, "http://192.168.1.83/test.php",job,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.v("Response", response.toString());
+                        try {
+                            Log.v("typeOfAction Log", response.getString("typeOfAction").toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.v("Error", error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+
+        };
+
+        // Adding request to request queue
+        RequestQueue rq = Volley.newRequestQueue(this);
+        rq.add(req);
+
+    }
 }
